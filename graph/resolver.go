868fc36919936933c71c7510da399f03a49faef4
tomotakashimizu <tomotaka.developer@gmail.com>
2023-03-25 23:33:29 +0900

    Initial commit
    https://gqlgen.com/getting-started/
    https://github.com/tomotakashimizu/gqlgen-todos

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+//go:generate go run github.com/99designs/gqlgen generate
+
+import "github.com/tomotakashimizu/todoapp-graphql-go-react/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
