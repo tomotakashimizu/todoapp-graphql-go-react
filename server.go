b0d85edc7643a4080e1770b933f74ee696088a4a
tomotakashimizu <tomotaka.developer@gmail.com>
2023-03-26 02:18:52 +0900

    postgresql DB に接続

--- server.go
+++ server.go
@@ -7,12 +7,26 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
 	"github.com/tomotakashimizu/todoapp-graphql-go-react/graph"
+	"xorm.io/xorm"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	connectionString := "user=test_user password=testtest dbname=test_db host=localhost port=5432 sslmode=disable"
+	engine, err := xorm.NewEngine("postgres", connectionString)
+	if err != nil {
+		log.Fatalln("error - create engine: ", err)
+	}
+
+	err = engine.Ping()
+	if err != nil {
+		log.Fatalln("error - connect DB: ", err)
+	}
+	log.Println("success - connect DB")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
