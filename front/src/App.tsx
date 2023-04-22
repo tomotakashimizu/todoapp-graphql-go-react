21ed4f4274b1f0d2057f6a86a29b02d5f4143790
tomotakashimizu <tomotaka.developer@gmail.com>
2023-04-22 18:48:46 +0900

    TodoListコンポーネントを作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
+      <TodoList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ front/src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useGetAllTodosQuery } from '../types/gen/api';
+
+export const TodoList: React.FC = () => {
+  const { data, loading, error } = useGetAllTodosQuery();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <ul>
+      {data?.todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
